18677c1e0dff622eb4d37180b7bb0e7ff9db18be
Alejandro Pernin <ale.pernin@gmail.com>
2022-07-08 16:42:21 -0300

    Ultimo cambio

--- common/health_check/monitored.py
+++ common/health_check/monitored.py
@@ -22,7 +22,7 @@ class MonitoredMixin(object):
         channel.exchange_declare(exchange='health_check', exchange_type='topic', auto_delete=True)
         try:
             while True:
-                logger.debug("%s Sending Heartbeat", self.node_name)
+                #logger.debug("%s Sending Heartbeat", self.node_name)
                 msg = json.dumps({'source': self.node_name})
                 channel.basic_publish(exchange='health_check', routing_key='health_check', body=msg)
                 time.sleep(HEALTHBEAT_DELAY)
